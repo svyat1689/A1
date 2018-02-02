5492475ecc1dc48326d4ef06c509728248e81be8
Svyatoslav Loboda <svyat1689@gmail.com>
2018-02-01 22:02:57 -0500

    some comments modified and added

--- src/FibonacciSeriesOfNumbers.java
+++ src/FibonacciSeriesOfNumbers.java
@@ -1,12 +1,16 @@
 /* Svyatoslav Loboda
  * Jan 25, 2018
- * This program outputs Fibonacci series of numbers using recursive and iterative functions.
+ * This program will produce the Fibonacci series of numbers output up to a predefined n
+ * (input by the user) using recursive and iterative functions.
  * 
  */
+
+//import a simple text scanner
 import java.util.Scanner;
 
 public class FibonacciSeriesOfNumbers {
 
+	//--- the main method -----------------------------------------------------------------------------------------
 	public static void main(String[] args) {
 		
 		Scanner reader = new Scanner(System.in);
@@ -17,18 +21,47 @@ public class FibonacciSeriesOfNumbers {
 		System.out.println("");
 		System.out.println("Fibonacci series of numbers using recursive function:");
 
-		
-		for (int i = 0; i <= num; i++ ) {
+        //utilizing a variable to get the system time before we execute the recursive factorial method
+        long startTimeRec =  System.nanoTime();
+        
+		for (int i = 0; i < num; i++ ) {
 			System.out.print(fiboSeriesRec(i)+" ");
 		}
 		
+        long endTimeRec = System.nanoTime();
+        //getting the time it took to run the method by subtracting startTime from endTime.
+        
+        long timeElapsedRec = endTimeRec-startTimeRec;
+        //outputting time elapsed
+		
+		System.out.println(" \nElapsed time in nanoseconds is: " + timeElapsedRec);
+        
 		System.out.println("");
 		System.out.println("");
 		System.out.println("Fibonacci series of numbers using iterative function:");
+        //utilizing a variable to get the system time before we execute the recursive factorial method
+       
+		long startTimeIte =  System.nanoTime();
 		
 		fiboSeriesIte(num);
 
+        long endTimeIte = System.nanoTime();
+        //getting the time it took to run the method by subtracting startTime from endTime.
+        
+        long timeElapsedIte = endTimeIte-startTimeIte;
+        //outputting time elapsed
+		
+		System.out.println(" \nElapsed time in nanoseconds is: " + timeElapsedIte);
+		
+		
+		System.out.print("Press any key to close the program: ");
+		Scanner userInput = new Scanner(System.in);
+		String input = userInput.nextLine();
+		userInput.close();
+
 	}
+	//--- the end of the main method ------------------------------------------------------------------------------
+	
 	
 	public static long fiboSeriesRec(int number) {
 		if (number == 0) {
@@ -45,7 +78,7 @@ public class FibonacciSeriesOfNumbers {
 	public static void fiboSeriesIte(int number) {
 		long secondLastNum = 0;
 		long lastNum = 1;
-		for (int i = 0; i <= number; i++) {
+		for (int i = 0; i < number; i++) {
 			long currentNum = 0;
 			if (i == 0) {
 				System.out.print(secondLastNum+" ");
@@ -60,6 +93,6 @@ public class FibonacciSeriesOfNumbers {
 				lastNum = currentNum;
 			}
 		}
-	}
+	}	
 
 }
